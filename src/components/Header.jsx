b4e870b70e7b8df5c94aba6ce376b38010457883
teo009 <teo.obando09@gmail.com>
2022-05-16 10:01:48 -0600

    [FIXED], images in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Image from 'next/image';
 import Menu from '@components/Menu';
 import MyOrder from '@containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
@@ -13,9 +14,9 @@ const Header = () => {
 	return (
 		<>
 			<nav className={styles.Nav}>
-				<img src={menu} alt="menu" className={styles.menu} />
+				<Image src={menu} alt="menu" className={styles.menu} />
 				<div className={styles['navbar-left']}>
-					<img src={logo} alt="logo" className={styles['nav-logo']} />
+					<Image src={logo} alt="logo" className={styles['nav-logo']} />
 					<ul>
 						<li>
 							<a href="/">All</a>
@@ -53,7 +54,7 @@ const Header = () => {
 							className={styles['navbar-shopping-cart']}
 							onClick={() => toggleOrder()}
 						>
-							<img 
+							<Image 
 								className={`
 									${styles['more-clickable-area']} 
 									${styles['pointer']}
